ec3a900caa1b18e1afa904ce583b280d36b544bd
Sky <7sky.js@gmail.com>
2023-04-13 20:34:13 +0200

    Added sorting and uploading
    Added sorting and uploading.

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
-require('dotenv').config();
-const express = require('express');
-const bcrypt = require('bcrypt');
-const { MongoClient } = require('mongodb');
+require("dotenv").config();
+const express = require("express");
+const upload = require("express-fileupload");
+const bcrypt = require("bcrypt");
+const { MongoClient } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 4444;
 const url = process.env.URL;
 const dbName = process.env.DB_NAME;
-const collectionName = process.env.COLLECTION_NAME;
+const collectionFiles = process.env.COLLECTION_FILES;
+const collectionTeachers = process.env.COLLECTION_TEACHERS;
 
 // Connect to MongoDB
 const client = new MongoClient(url);
+const collectionF = client.db(dbName).collection(collectionFiles);
+const collectionT = client.db(dbName).collection(collectionTeachers);
 
 async function connectToDatabase() {
   try {
     await client.connect();
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
   } catch (err) {
     console.log(`Error connecting to MongoDB: ${err}`);
   }
@@ -23,43 +27,68 @@ async function connectToDatabase() {
 connectToDatabase();
 
 // Set up Express
-app.use(express.static(__dirname + '/public'));
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
+app.use(upload());
 
 // Routes
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/login.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/login.html");
 });
 
 let isLoggedIn = false;
 
-app.post('/login', (req, res) => {
-    isLoggedIn = false;
+app.post("/login", (req, res) => {
+  isLoggedIn = false;
   const password = req.body.password;
   bcrypt.compare(password, process.env.HASH, (err, result) => {
     if (result) {
       isLoggedIn = true;
-      res.redirect('/successful');
+      res.redirect("/successful");
     } else {
-      res.sendFile(__dirname + '/public/error.html');
+      res.sendFile(__dirname + "/public/error.html");
     }
   });
 });
 
-app.get('/successful', async (req, res) => {
+app.get("/successful", async (req, res) => {
   if (isLoggedIn) {
     try {
-      const collection = client.db(dbName).collection(collectionName);
-      const data = await collection.find().toArray();
-      res.render('index.ejs', { data });
+      const dataFiles = await collectionF.find().sort({ teacher: 1 }).toArray();
+      const dataTeachers = await collectionT.find().toArray();
+      res.render("index.ejs", { dataFiles, dataTeachers });
     } catch (err) {
       console.log(`Error retrieving data from MongoDB: ${err}`);
-      res.sendFile(__dirname + '/public/error.html');
+      res.sendFile(__dirname + "/public/error.html");
     }
   } else {
-    res.sendFile(__dirname + '/public/error.html');
+    res.sendFile(__dirname + "/public/error.html");
+  }
+});
+
+app.get("/sort", async (req, res) => {
+  const dataTeachers = await collectionT.find().toArray();
+  const { teacher } = req.query;
+  const dataSorted = await collectionF.find({ teacher: teacher }).toArray();
+  res.render("sorted.ejs", { dataSorted, dataTeachers });
+});
+
+app.post("/add", (req, res) => {
+  const teacher = req.body.teacher;
+  if (req.files) {
+    const file = req.files.fileName;
+    const fileName = file.name;
+    file.mv(__dirname + "/public/files/" + fileName, fileName);
+    collectionF.insertOne({
+      name: `${fileName}`,
+      teacher: `${teacher}`,
+    });
+    isLoggedIn = true;
+    res.redirect("/successful");
+  } else {
+    res.sendFile(__dirname + "/public/error.html");
   }
 });
 
